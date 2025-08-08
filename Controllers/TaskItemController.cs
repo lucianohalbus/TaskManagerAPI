9e613e49da9b5ea123b975c65a5e6aaa5f7ba50c
Edutesc <luciano.puzer@ufabc.edu.br>
2025-08-08 15:05:40 -0300

    implementing SQL Server

---
+++ Controllers/TaskItemController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskItemController : ControllerBase
+    {
+        private readonly TaskManagerContext _context;
+
+        public TaskItemController(TaskManagerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TaskItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        // GET: api/TaskItem/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(int id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+            if (taskItem == null) return NotFound();
+            return taskItem;
+        }
+
+        // POST: api/TaskItem
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem taskItem)
+        {
+            _context.TaskItems.Add(taskItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTaskItem), new { id = taskItem.Id }, taskItem);
+        }
+
+        // PUT: api/TaskItem/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTaskItem(int id, TaskItem taskItem)
+        {
+            if (id != taskItem.Id) return BadRequest();
+            _context.Entry(taskItem).State = EntityState.Modified;
+            try { await _context.SaveChangesAsync(); }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.TaskItems.Any(e => e.Id == id)) return NotFound();
+                else throw;
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/TaskItem/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTaskItem(int id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+            if (taskItem == null) return NotFound();
+            _context.TaskItems.Remove(taskItem);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using TaskManagerApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// *** Adicione esta parte para registrar o contexto! ***
+builder.Services.AddDbContext<TaskManagerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
